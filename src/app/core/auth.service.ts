f721d0d41443cbb3194645de32b72167a883e31e
ivani-k <103540161+ivani-k@users.noreply.github.com>
2022-04-29 13:00:58 +0300

    Add files via upload

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+//import { IUser } from '../interfaces/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  setUser(data: any) {
+    localStorage.setItem('user', JSON.stringify(data));    
+  }
+
+  getUser(){
+    const user = localStorage.getItem('user');
+    if(user) {
+      return JSON.parse(user);
+    } 
+  }
+
+  removeUser() {
+    localStorage.removeItem('user');
+  }
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+//import { AuthActivate } from '../guards/auth.activate';
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  providers: [
+    //AuthActivate
+  ]
+})
+export class CoreModule { }
